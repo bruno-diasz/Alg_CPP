d37f8a21fcd73860f3650a8f50eb260c069906cb
Bruno <brunoipb1@gmail.com>
2025-04-28 01:34:22 +0000

    first commit

---
+++ L1/q1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+string nome_a,nome_b;
+float altura_a,altura_b;
+char sangue_a , sangue_b;
+int idade_a, idade_b;
+
+cin >> nome_a;
+cout<<  nome_a << endl;
+return 0;
+}
