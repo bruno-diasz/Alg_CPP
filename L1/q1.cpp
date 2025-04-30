38bee39fc8690c77e787b0f7c1bf0c04500c2a64
Bruno <brunoipb1@gmail.com>
2025-04-30 00:47:37 +0000

    Atualizando Q1

--- L1/q1.cpp
+++ L1/q1.cpp
@@ -2,12 +2,28 @@
 using namespace std;
 
 int main(){
-string nome_a,nome_b;
-float altura_a,altura_b;
-char sangue_a , sangue_b;
-int idade_a, idade_b;
+    string nome, cor;
+    float altura,peso;
+    char sangue , cnh;
+    int idade,numero_irmaos;
 
-cin >> nome_a;
-cout<<  nome_a << endl;
-return 0;
+    cout<< "Digite seu nome" << endl;
+    cin >> nome ;
+    cout<< "Digite sua cor" << endl;
+    cin >> cor ;
+    cout<< "Digite sua altura " << endl;
+    cin >> altura ;
+    cout<< "Digite seu peso" << endl;
+    cin >> peso ;
+    cout<< "Digite seu tipo Sanguineo" << endl;
+    cin >> sangue ;
+    cout<< "Digite sua categoria de CNH" << endl;
+    cin >> cnh ;
+    cout<< "Digite sua idade" << endl;
+    cin >> idade ;
+    cout<< "Digite seu numero de irmãos" << endl;
+    cin >> numero_irmaos ;
+    
+  
+    return 0;
 }
